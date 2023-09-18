3ebb902f6d58038a5902561011a22053cbaac5e7
roybrockington <r.brockington@gmail.com>
2023-09-18 16:33:15 +0100

    Blur styling

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -2,7 +2,7 @@
   export let price;
 </script>
 
-<div class="my-2 flex flex-col bg-zinc-200 w-full md:w-3/4 lg:w-1/2 gap-3 rounded-xl py-4 px-6 border border-zinc-400">
+<div class="opacity-90 bg-white/20 backdrop-blur-md my-2 flex flex-col w-full md:w-3/4 lg:w-1/2 gap-3 rounded-xl py-4 px-6 border border-zinc-400 text-white">
   <span class="text-md font-bold">
     {price.description.split(" - ")[0]}
   </span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import prices from '../prices.json'
   import { createSearchStore, searchHandler } from '$lib/stores/search'
-  import {onDestroy } from 'svelte'
+  import { onDestroy } from 'svelte'
   import type { PageData } from './$types'
   import Product from '../components/Product.svelte'
+//  import Logo from '../components/Logo.svelte'
 
   type Product = {
     title: string
@@ -19,16 +20,24 @@
     searchTerms: `${product.description} ${product.sku} ${product.category}`
   }))
 
-
   const searchStore = createSearchStore(searchProducts)
   const unsubscribe = searchStore.subscribe((model) => searchHandler(model))
   
   onDestroy(()=> {
     unsubscribe()
   })
+
 </script>
 
-<div class="flex flex-col w-full items-center">
+<div class="
+  flex
+  flex-col
+  w-full
+  items-center
+  back-drop
+  "
+>
+
   <h1 class="text-3xl my-5 font-bold text-center">
     Hello Ascot!
   </h1>
@@ -38,15 +47,22 @@
     class="w-1/2 mx-4 p-2 rounded-xl border-zinc-400"
     bind:value={$searchStore.search}
   />
+  <div class="flex flex-col w-full items-center px-3 mt-4">
+    {#each $searchStore.filtered as price}
+      <Product {price} />
+    {/each}
+  </div>
 </div>
-<div class="flex flex-col w-full items-center px-3 mt-4">
-  {#each $searchStore.filtered as price}
-    <Product {price} />
-  {/each}
-</div>
-
 <style lang="postcss">
   :global(html) {
     background-color: theme(colors.gray.100);
+  }
+  
+  .back-drop {
+    background-color: #000;
+    background-image: linear-gradient(
+      to bottom, transparent, #000
+    ), url("https://hifishowlive.com/sites/default/files/slider/homeimage1a.jpg");
+    background-repeat: no-repeat;
   }
 </style>
