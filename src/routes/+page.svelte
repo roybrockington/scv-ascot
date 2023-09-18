47c00a36afa0ee6a5362882fee49934913e99d95
roybrockington <r.brockington@gmail.com>
2023-09-18 14:33:28 +0100

    Add search functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script>
   import prices from '../prices.json'
+  import { createSearchStore, searchHandler } from '$lib/stores/search'
+  import {onDestroy } from 'svelte'
   import Product from '../components/Product.svelte'
+
+  const searchProducts = prices.map((product) => ({
+    ...product,
+    searchTerms: `${product.description} ${product.sku} ${product.category}`
+  }))
+
+  const searchStore = createSearchStore(searchProducts)
+  const unsubscribe = searchStore.subscribe((model) => searchHandler(model))
+  
+  onDestroy(()=> {
+    unsubscribe()
+  })
 </script>
+
 <div class="flex flex-col w-full items-center">
   <h1 class="text-3xl my-5 font-bold text-center">
     Hello Ascot!
   </h1>
-  <input type="text" placeholder="search products..." class="w-1/2"/>
+  <input type="search" placeholder="search products..." class="w-1/2" bind:value={$searchStore.search} />
 </div>
 <div class="flex flex-col w-full items-center px-3">
-  {#each prices as price}
+  {#each $searchStore.filtered as price}
     <Product {price} />
   {/each}
 </div>
