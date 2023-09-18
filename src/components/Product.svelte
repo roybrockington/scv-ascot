85d23453d3c455fcc5f494a80c34a018cf145f61
roybrockington <r.brockington@gmail.com>
2023-09-18 13:48:38 +0100

    Add product component & json

---
+++ src/components/Product.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let price;
+</script>
+
+<div class="my-2 flex flex-col bg-zinc-200 w-full md:w-3/4 lg:w-1/2 items-center gap-3 rounded-xl py-4 text-center">
+  <span class="text-md font-bold">{price.description}</span>
+  <span class={`${price.ascot ? 'line-through text-lg': 'text-3xl'}`}>£{price.ssp.toFixed(2)}</span>
+  <span class={`${price.ascot ? 'text-3xl': 'hidden'}`}>
+    £{price.ascot}
+  </span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
-<h1 class="text-3xl font-bold underline">
-  Hello Ascot!
-</h1>
+<script>
+  import prices from '../prices.json'
+  import Product from '../components/Product.svelte'
+</script>
+<div class="flex flex-col w-full items-center">
+  <h1 class="text-3xl my-5 font-bold text-center">
+    Hello Ascot!
+  </h1>
+  <input type="text" placeholder="search products..." class="w-1/2"/>
+</div>
+<div class="flex flex-col w-full items-center px-3">
+  {#each prices as price}
+    <Product {price} />
+  {/each}
+</div>
 
 <style lang="postcss">
   :global(html) {
